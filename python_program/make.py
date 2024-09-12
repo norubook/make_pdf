a3a9e1175836812b6d6ff48c335d2338ef34f6bb
norubook <nisshi5114@gmail.com>
2024-09-12 14:34:15 +0900

    first commit

---
+++ python_program/make.py
@@ -0,0 +1,44 @@
+import tkinter,os
+from tkinter import filedialog
+
+file = filedialog.askopenfilename(filetypes=[("","*")])
+out_path = os.path.abspath(os.path.join(file, os.pardir))+'/'
+
+fr = open(file,'r', encoding='UTF-8')
+fr_txt= fr.read()
+fr.close()
+a=0
+b="title"
+c="main"
+output='''
+<!DOCUTYPE html>
+<html>
+<head>
+<div id="title">
+{}
+</div>
+<style>
+</style>
+</head>
+<body>
+<h1>
+<section class="main"><p>{}</p>
+</section>
+</h1>
+</body>
+</html>
+'''.format(b,fr_txt)
+while 1:
+ try:
+  if a==0:
+   f = open('{}test.html'.format(out_path), 'x', encoding='UTF-8')
+   break
+  else:
+   f = open('{}test_{}.html'.format(out_path,str(a)), 'x', encoding='UTF-8')
+   break
+
+ except:
+  a+=1
+
+f.write(output)
+f.close()
